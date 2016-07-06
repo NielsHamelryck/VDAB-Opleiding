d83e98d62779326e9c8d89d8483b1b5948970a8e
joeripardon <joeri.pardon1@telenet.be>
2016-07-06 15:55:21 +0200

    volledig aangepaste test html + start ado .net

---
+++ CsharpADO/AdoCursus/AdoGemeenschap/RekeningenManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace AdoGemeenschap
+{
+    public class RekeningenManager
+    {
+        public Int32 SaldoBonus()
+        {
+            var dbmanager = new BankDbManager();
+            using (var conBank = dbmanager.GeConnection())
+            {
+                using (var comBonus = conBank.CreateCommand())
+                {
+                    comBonus.CommandType = CommandType.Text;
+                    comBonus.CommandText = "update Rekeningen set Saldo=Saldo*1.1";
+                    conBank.Open();
+                    return comBonus.ExecuteNonQuery();
+                }
+            }
+
+        }
+    }
+}
---
+++ CsharpADO/AdoCursus/AdoWPF/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data.Common;
+using AdoGemeenschap;
+
+namespace AdoWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBieren_OnClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var manager = new BierenDbManager();
+                
+                using (var conBieren = manager.GetConnection())
+                {
+
+                    
+                    
+                    conBieren.Open();
+                    LabelStatus.Content = "Bieren geopend";
+                }
+            }
+            catch (Exception ex)
+            {
+                LabelStatus.Content = ex.Message;
+            }
+        }
+
+        private void ButtonBonus_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var manager = new RekeningenManager(); 
+                LabelStatus.Content = manager.SaldoBonus() + " rekeningen aangepast";
+            }
+            catch (Exception ex)
+            {
+                LabelStatus.Content = ex.Message; 
+            }
+                    }
+    }
+}
